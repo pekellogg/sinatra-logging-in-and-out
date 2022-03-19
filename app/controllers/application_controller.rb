04f4c8c341ca1882d57cb339a055c287326a61d1
Peyton <pekellogg@hotmail.com>
2022-03-19 14:13:20 -0700

    commit tree d541d74d53974e22e9d55ff770f4f237cddddf92

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,43 @@
 require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
-    set :session_secret, "secret"
+    set :session_secret, ".;zH6W$jMBRKyt])"
   end
 
+  # READ
+  # LOGIN FORM
   get '/' do
     erb :index
   end
 
+  # CREATE
+  # LOGIN FORM
   post '/login' do
-
+    @user = User.where(username: params[:username], password: params[:password]).first
+    if @user
+      # if successful login, set session's user id to the logged in user's id
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
+  # READ
   get '/account' do
-
+    @user = User.find_by(id: session[:user_id])
+    if @user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+    def self.is_logged_in?(session)
+        current_user(session) ? true : false
+    end
+
+    def self.current_user(session)
+        @user = User.find_by_id(session[:user_id])
+    end
 end
